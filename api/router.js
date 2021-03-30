2b85934e4216ef7b0d657c1ab96d93a6ca320924
jmurphy0 <murphbot55@gmail.com>
2021-03-30 16:25:58 -0400

    working on chat elements

--- api/router.js
+++ api/router.js
@@ -1,11 +1,12 @@
-require( 'dotenv' ).config()
+const dotenv = require("dotenv").config();
 
-const router = require('express').Router();
-const mongoose = require('mongoose')
-const bcrypt = requite('bcrypt')
-const jwt = require('jsonwebtoken')
-const Paws = require('../models/index.js');
-const User = require('../models/index.js')
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Paws = require("../models/index.js");
+const Messages = require("../models/index.js");
+const User = require("../models/index.js");
 
 // router.get('/api/books', (req,res) => {
 //     Paws.find({})
@@ -17,107 +18,106 @@ const User = require('../models/index.js')
 //       });
 // })
 
-
-
-router.post('/signup', (req, res, next) => {
-    User.find({email: req.body.eamil})
-        .exec()
-        .then(user => {
-            if (user.length > 0) {
-                return res.status(409).json({
-                    message: 'Mail exists'
-                })
-            } else {
-            bcrypt.hash(req.body.email, 10, (err, hash) => {
-                if (err) {
-                    return res.status(500).json({
-                        error: err
-                    })
-                } else {
-                    const user = new User({
-                        _id: mongoose.Schema.Types.ObjectId(),
-                        email: req.body.email,
-                        password: hash
-                    })
-                    user
-                        .save()
-                        .then( result => {
-                            console.log(result)
-                            res.status(201).json({
-                                message: 'User created'
-                            })
-                        })
-                        .catch(err => {
-                            console.log(err);
-                            res.status(500).json({
-                                error: err
-                            })
-                        })
-                    }
-                })
-            }
-        })
-        .catch();
-})
-
-router.post('/login', (req,res,next) => {
-    User.find({ email: req.body.email })
-    .exect()
-    .then(user => {
-        if (user.length < 1) {
-            return res.status(401).json({
-                message: 'Auth failed'
-            })
-        }
-        bcrypt.compare(req.body.password, user[0].password, (err,result) => {
-            if (err) {
-                return res.status(401).json({
-                    message: 'Auth failed'
-                })
-            }
-            if (result) {
-                const token = jwt.sign({
-                    email: user[0].email,
-                    userId: user[0]._id
-                }, process.env.JWT_KEY, 
-                {
-                    expiresIn: "1h"
-                })
-                return res.status(200).json({
-                    message: 'Auth successful',
-                    token: token
-                })
-            }
-            res.status(401).json({
-                message: 'Auth failed'
-            })
-            })
-        
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        })
-    })
-})
-
-router.delete('/:userId', (req,res,next) => {
-    User.remove({_id: req.params.userId})
+router.post("/signup", (req, res, next) => {
+  User.find({ email: req.body.eamil })
     .exec()
-    .then(result => {
-        res.status(200).json({
-            message: "User deleted"
-        })
+    .then((user) => {
+      if (user.length > 0) {
+        return res.status(409).json({
+          message: "Mail exists",
+        });
+      } else {
+        bcrypt.hash(req.body.email, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err,
+            });
+          } else {
+            const user = new User({
+              _id: mongoose.Schema.Types.ObjectId(),
+              email: req.body.email,
+              password: hash,
+            });
+            user
+              .save()
+              .then((result) => {
+                console.log(result);
+                res.status(201).json({
+                  message: "User created",
+                });
+              })
+              .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                  error: err,
+                });
+              });
+          }
+        });
+      }
     })
-    .catch(err => {
-        console.log(err)
-        res.status(500).json({
-            error: err
-        })
-    })
-})
+    .catch();
+});
 
+router.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exect()
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth failed",
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h",
+            }
+          );
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token,
+          });
+        }
+        res.status(401).json({
+          message: "Auth failed",
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+router.delete("/:userId", (req, res, next) => {
+  User.remove({ _id: req.params.userId })
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "User deleted",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
 
 // router.post('/api/books', (req,res) => {
 //     Paws.create({})
@@ -129,4 +129,4 @@ router.delete('/:userId', (req,res,next) => {
 //       })
 // })
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,45 @@
-require( 'dotenv' ).config()
+require("dotenv").config();
 
-const path = require('path')
-const express = require('express');
+const { user, Paws } = require("./models/index");
+const path = require("path");
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Messages = require("./models/paws.js");
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('./client/build') );
+app.use(express.static("./client/build"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/paws", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
-app.use(require('./api/router.js'))
+app.use(require("./api/router.js"));
 
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
   console.log("[HTML GET]: Get React app");
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log( `Listening on port ${PORT}` )
-})
+app.get("/", (req, res) => res.status(200).send("helloworld"));
+
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(`new message created: \n ${data}`);
+    }
+  });
+});
+
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+});
