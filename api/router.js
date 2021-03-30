06b583f3fe5d73210b78ac40ac95ce00481abba6
T__R__P__53 <tyler_1701@hotmail.com>
2021-03-30 13:25:58 -0400

    signup, login, token

--- api/router.js
+++ api/router.js
@@ -1,6 +1,9 @@
+require( 'dotenv' ).config()
+
 const router = require('express').Router();
 const mongoose = require('mongoose')
 const bcrypt = requite('bcrypt')
+const jwt = require('jsonwebtoken')
 const Paws = require('../models/index.js');
 const User = require('../models/index.js')
 
@@ -73,8 +76,16 @@ router.post('/login', (req,res,next) => {
                 })
             }
             if (result) {
+                const token = jwt.sign({
+                    email: user[0].email,
+                    userId: user[0]._id
+                }, process.env.JWT_KEY, 
+                {
+                    expiresIn: "1h"
+                })
                 return res.status(200).json({
-                    message: 'Auth successful'
+                    message: 'Auth successful',
+                    token: token
                 })
             }
             res.status(401).json({
