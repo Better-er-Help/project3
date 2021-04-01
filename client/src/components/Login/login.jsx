7277766ab39ff57fcd34f2d2b1473e3ee3f32181
T__R__P__53 <tyler_1701@hotmail.com>
2021-04-01 13:31:08 -0400

    test

--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -1,5 +1,7 @@
 import {Link} from 'react-router-dom';
+import { useRef, useState } from 'react'
 import Smiley from '../imgs/AwesomeFace.png'
+import './login.css'
 
 function LoginForm(){
 
@@ -11,39 +13,39 @@ function LoginForm(){
             <Link to='/'>Chat</Link>
             <Link to='/signup'>Sign-up</Link>
           </div>
-                <button type="button" className="btn float-end" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <button type="button" className="btn float-end" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     Log in
                 </button>
         </div>
 
         <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">Log in</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
-                
-                <div className="container d-flex justify-content-center">
-                  <form>
-                    <div className="mb-3" id='heroo'>
-                      <label htmlFor="enteruse" className="form-label">Username</label>
-                      <input className="form-control" id="enteruse"/>
-                    </div>
-                    <div className="mb-3" id='again'>
-                      <label htmlFor="enterpass" className="form-label">Password</label>
-                      <input type="password" className="form-control" id="enterpass"/>
-                    </div>
-                    <a className="signup">Sign up</a>
-                    <button className="btn btn-primary float-end">Log in</button>
-                  </form>
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Log in</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                  
+                  <div className="container d-flex justify-content-center">
+                    <form>
+                      <div className="mb-3">
+                        <label htmlFor="enteruse" className="form-label">Username</label>
+                        <input className="form-control"/>
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="enterpass" className="form-label">Password</label>
+                        <input type="password" className="form-control"/>
+                      </div>
+                      <a className="signup" href="sign.html">Sign up</a>
+                      <button className="btn btn-primary float-end">Log in</button>
+                    </form>
                   </div>
 
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </>
     )
 }
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 require("dotenv").config();
 
-const { user, Paws } = require("./models/index");
 const path = require("path");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Messages = require("./models/paws.js");
 
 const PORT = process.env.PORT || 3001;
 
@@ -25,19 +23,6 @@ app.use(require("./api/router.js"));
 app.get("*", (req, res) => {
   console.log("[HTML GET]: Get React app");
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.get("/", (req, res) => res.status(200).send("helloworld"));
-
-app.post("/messages/new", (req, res) => {
-  const dbMessage = req.body;
-  Messages.create(dbMessage, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(201).send(`new message created: \n ${data}`);
-    }
-  });
 });
 
 app.listen(PORT, function () {
