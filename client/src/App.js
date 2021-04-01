94e18463a064939eff6a4625a95043c23f792881
jmurphy0 <murphbot55@gmail.com>
2021-04-01 11:52:23 -0400

    fix server and app

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,53 @@ import Chat from "./components/Chat";
 import Signup from "./components/Signup/signup";
 import Login from "./components/Login/login";
 import "./App.css";
-import ChatPage from "./pages/ChatPage";
+import axios from "./axios";
+// import axios from "axios";
 
 function App() {
+  //settign states
+  const [messages, setMessages] = useState([]);
+
+  //setting axios calls to get messages from db
+  useEffect(async () => {
+    const res = await axios.get("/messages");
+    setMessages(res.data);
+  }, []);
+
+  //  useEffect(() => {
+  //    async function getData() {
+  //      const data = await axios.get("/messages/sync");
+  //      setMessages(data);
+  //    }
+  //  }, []);
+
+  // this is the realtime mongo hookup.
+  console.log(messages);
+  useEffect(() => {
+    const pusher = new Pusher("19b49e3760d87d26f1b4", {
+      cluster: "us2",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", function (data) {
+      alert(JSON.stringify(data));
+      setMessages([...messages, data]);
+    });
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+  console.log(messages);
+
   return (
     <>
       <Router>
         <Login />
         <div className="app">
           <div className="appbody">
-            <Route exact path="/" component={ChatPage} />
+            <Chat messages={messages} />
+            {/* <Route exact path="/" component={Chat({ messages })} /> */}
             <Route exact path="/signup" component={Signup} />
           </div>
         </div>
