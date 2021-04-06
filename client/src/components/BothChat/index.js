512beb7a7380b63fb92388a37bb03a237c6699fc
jmurphy0 <murphbot55@gmail.com>
2021-04-06 12:11:28 -0400

    fixed icons in chat

--- client/src/components/BothChat/index.js
+++ client/src/components/BothChat/index.js
@@ -140,7 +140,10 @@ function BothChat({ messages }) {
             })}
           </div>
           <div className="chatFooter">
-            <InsertEmoticon />
+            <IconButton>
+              <InsertEmoticon />
+              <MicIcon />
+            </IconButton>
             <form>
               <input
                 value={input}
@@ -157,7 +160,6 @@ function BothChat({ messages }) {
                 <Send />
               </IconButton>
             </form>
-            <MicIcon />
           </div>
         </div>
       </>
@@ -218,7 +220,10 @@ function BothChat({ messages }) {
             })}
           </div>
           <div className="chatFooter">
-            <InsertEmoticon />
+            <IconButton>
+              <InsertEmoticon />
+              <MicIcon />
+            </IconButton>
             <form>
               <input
                 value={input}
@@ -235,7 +240,6 @@ function BothChat({ messages }) {
                 <Send />
               </IconButton>
             </form>
-            <MicIcon />
           </div>
         </div>
         <div style={{ display: "none" }} ref={nameRef}>
