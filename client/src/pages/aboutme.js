5f23bc71d00d29610b9d0fca7f5f825da8608d1d
muhammad alim <muhammadalim3435@gmail.com>
2021-04-06 11:47:47 -0400

    aboutme page

---
+++ client/src/pages/aboutme.js
@@ -0,0 +1,113 @@
+function Aboutme() {
+    return (
+        <div className="container bg-gray">
+
+            <div className="row about-boxes">
+                <div className="col-sm-12 box"></div>
+                <div id="our-mission">
+                    <h3>Our Mission</h3>
+                    <div className="col-sm-offset-3 center">
+                        <p>
+                            "Making professional counseling accessible, affordable,
+                            convenient-so anyone who struggle with life's challenges can get help,
+                            anytime,anywhere"
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row about-boxes">
+                <div className="col-sm-12">
+                    <div id="our-counselors">
+                        <h3>Our Counselors</h3>
+                        <div className="row">
+                            <div className="col-sm-8 col-sm-offset-2">
+                                <p className="center">
+                                    "BetterHelp offers access to licensed, trained, experienced, and
+                                    accredited psychologists (Phd/PsyD), marriage and family therapists
+                                    (LMFT), clinical social workers(LCSW/LMSW), and board licensed
+                                    professional counselors(LPC)"
+                            </p>
+                            </div>
+                        </div>
+                        <div className="col-xs-12 center-button">
+                            <a href="/counselors/" className="second-cta">Meet our counselors</a>
+                        </div>
+                    </div>
+                    <div className="row about-boxes">
+                        <div className="col-sm-12">
+                            <div id="our-success-stories">
+                                <h3>Our Success Stories</h3>
+                                <div className="row">
+                                    <div className="col-sm-10-offset-1 center mtop20">
+                                        <div className="col-sm-4">
+                                            <div className="success-story">
+                                                <img src="insert image">
+                                                    "The biggest achievement Ive made is, truly,fighting for myself.
+                                                    Through working with Aiko and taking the time to discuss various issues with her, Ive become
+                                                    become a bigger advocate for myself, and im so proud of that."
+
+                                                    <div>
+                                                        <a href="insert link">Read more</a>
+                                                    </div>
+                                                        </div>
+                                            </div>
+                                            <div className="success-story">
+                                                <img src="insert img link">
+                                                    "I have now been working with Sheilah on BetterHelp fo abou 4 weeks, and can confidently say that
+                                                    I am a lot less anxious about my career path now than when I first signed up...I am
+                                                    so much better off now than I was before."
+                                                            </img>
+
+                                                <div>
+                                                    <a href="insert link here"></a>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="col-sm-4">
+                                            <div className="success-story">
+                                                <img src="insert img">
+                                                    "I am a totally different person now than when I signed up 19 months ago. I have found my
+                                                    my voice and can speak up for myself now....My confidence level is also better than it has
+                                                    ever been."
+                                                                        </img>
+
+                                                <div>
+                                                    <a href="insert link here">Read more</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="row">
+
+                                    <div className="col-xs-12 center-button mtop20">
+                                        <a href="insert link" className="second-cta">See more stories</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+
+
+
+        </div>
+                                        </div >
+                                    </div >
+                                </div >
+                            </div >
+                        </div >
+                    </div >
+                    
+            </div >
+        </div >
+
+    )
+}
+export default Aboutme
