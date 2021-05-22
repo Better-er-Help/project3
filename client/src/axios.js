74a6731a70571cc4b701dba467b6f80c0483f499
T__R__P__53 <tyler_1701@hotmail.com>
2021-05-22 08:34:20 -0400

    baseURL

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // change url when deployed to heroku
 const instance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "mongodb+srv://admin:paws12345@cluster0.nlzcv.mongodb.net/paws?retryWrites=true&w=majority",
 });
 
 export default instance;
